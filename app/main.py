2b6a9d9c42bf6a09557f633d5a4ca9650d3df91e
György Demarcsek <gdemarcsek@users.noreply.github.com>
2023-05-30 20:50:34 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -10,12 +10,6 @@ def read_root() -> Dict[str, str]:  # pragma: no cover
     return {"Hello": "World"}
 
 
-@app.get("/vulnerable/{my_param}")
-def vulnerable(my_param: str) -> int:  # pragma: no cover
-    import os
-    return os.system(my_param)
-
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int,
               q: Union[str, None] = None) -> Dict[str, Union[None, int, str]]:  # pragma: no cover
